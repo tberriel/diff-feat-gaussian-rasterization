38c2c076e1e08428e02c194d7ef12a43fc87ee65
tberriel <57666961+tberriel@users.noreply.github.com>
2024-05-26 18:22:58 +0200

    Modified package name

--- setup.py
+++ setup.py
@@ -15,18 +15,20 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="diff_feat_gaussian_rasterization",
+    packages=['diff_feat_gaussian_rasterization'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff_feat_gaussian_rasterization._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]
+            #,'cxx': ['-g']  Added to debug, remove to train
+            })
         ],
     cmdclass={
         'build_ext': BuildExtension
